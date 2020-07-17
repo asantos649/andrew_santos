05e29538fe3f162a325d521021394f09189fd8c6
asantos649 <asantos@alumni.nd.edu>
2020-07-17 11:07:47 -0400

    Updates

---
+++ precache-manifest.75ede810157222baae7bfd726e42fe01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3870b283fc67462ef0312a0c261e7f0d",
+    "url": "/andrew_santos/index.html"
+  },
+  {
+    "revision": "4a21ab77aff4d64b282d",
+    "url": "/andrew_santos/static/css/main.3ccbec63.chunk.css"
+  },
+  {
+    "revision": "00651371577a06ff7f45",
+    "url": "/andrew_santos/static/js/2.353f60a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/andrew_santos/static/js/2.353f60a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a21ab77aff4d64b282d",
+    "url": "/andrew_santos/static/js/main.c1775d6c.chunk.js"
+  },
+  {
+    "revision": "571dca66aaa8207f9693",
+    "url": "/andrew_santos/static/js/runtime-main.bc7fc1da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/andrew_santos/static/media/logo.5d5d9eef.svg"
+  }
+]);
