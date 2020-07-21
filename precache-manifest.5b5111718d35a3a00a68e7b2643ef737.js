86c1fbe5dd2f749f2e2553e63bcef33ee51b5657
asantos649 <asantos@alumni.nd.edu>
2020-07-21 14:38:42 -0400

    Updates

---
+++ precache-manifest.5b5111718d35a3a00a68e7b2643ef737.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733642fa8fbcae861d6ce6da86675762",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b375a90c571f2a00c8c",
+    "url": "/static/css/main.8ef16d81.chunk.css"
+  },
+  {
+    "revision": "00651371577a06ff7f45",
+    "url": "/static/js/2.353f60a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.353f60a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b375a90c571f2a00c8c",
+    "url": "/static/js/main.e45637f7.chunk.js"
+  },
+  {
+    "revision": "9617bb2260cd1a12f8df",
+    "url": "/static/js/runtime-main.d9281491.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/andrew_santos/precache-manifest.5d67c9034fc07d481ef69c9e95f424b3.js"
+  "/precache-manifest.5b5111718d35a3a00a68e7b2643ef737.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/andrew_santos/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
